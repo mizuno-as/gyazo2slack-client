c85a317f1cb18b23bda90cdcc4a691c75fa878a9
Hajime MIZUNO <mizuno-as@ubuntu.com>
2017-12-12 12:03:50 +0900

    initial version

---
+++ gyazo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+URL='https://API_ENDPOINT'
+
+TEMP=$(mktemp /tmp/XXXXX.png)
+WIDTH=800
+
+if [ $# -eq 0 ]; then
+    import ${TEMP}
+else
+    if [ $(identify -format %w ${1}) -gt ${WIDTH} ]; then
+        CONVOPT="-resize ${WIDTH}"
+    fi
+    convert ${CONVOPT} ${1} $TEMP
+fi
+
+IMAGE=$(curl -s -H 'Content-Type: image/png' --data-binary "@${TEMP}" -X POST ${URL})
+if [ $? -eq 0 ]; then
+    gnome-www-browser ${IMAGE}
+    echo ${IMAGE} | xclip -sel c
+fi
+rm -f ${TEMP}
